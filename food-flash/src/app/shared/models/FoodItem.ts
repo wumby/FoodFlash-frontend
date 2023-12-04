dc43fa8bb76ee7b0a2759ccc0ba9a0f89c62f9ba
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2023-12-04 11:12:03 -0700

    docker

--- food-flash/src/app/shared/models/FoodItem.ts
+++
@@ -1,9 +0,0 @@
-export interface FoodItem {
-    id?: number;
-    itemName?: string;
-    itemDescription?: string;
-    isVeg?: boolean;
-    price?: number;
-    restaurantId?: number;
-    quantity: number;
-}
--- food-flash/src/app/shared/url.ts
+++
@@ -1,4 +0,0 @@
-export const API_URL_R = 'http://localhost:9091';
-export const API_URL_ORDER = 'http://localhost:9094';
-export const API_URL_FC = 'http://localhost:9092';
-export const API_URL_UD = 'http://localhost:9093'
